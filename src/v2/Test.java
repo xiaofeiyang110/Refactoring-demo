af1dae95d28ef558ecc27e8c9a5cb9070c2735c9
yangshuai <yangshuai@howzhi.com>
2018-11-23 20:06:05 +0800

    start v2

---
+++ src/v2/Test.java
@@ -0,0 +1,24 @@
+package v2;
+
+import v2.Movie;
+import v2.Rental;
+import  v2.Customer;
+
+public class Test {
+
+    public static void  main(String[] args)
+    {
+        Movie m1 = new Movie("儿童1",Movie.CHILDRENS);
+        Movie m2 = new Movie("新片1",Movie.NEW_RELEASE);
+
+        Rental r1 = new Rental(m1,1);
+        Rental r2 = new Rental(m2,2);
+
+        Customer c = new Customer("张三");
+        c.addRental(r1);
+        c.addRental(r2);
+        String res = c.statement();
+
+        System.out.print(res);
+    }
+}
